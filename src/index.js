7ab96fe52fcff5c278f61e4de5876710cf8f18ff
vcordero07 <vcordero07@gmail.com>
2018-01-23 23:12:16 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 import store from './store';
 
 ReactDOM.render(
@@ -13,4 +13,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-registerServiceWorker();
